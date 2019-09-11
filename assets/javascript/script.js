434c997be776856f8c3032ebb8ed989ce5d9a8c4
race-briggs <race.briggs@gmail.com>
2019-09-11 15:26:05 -0500

    added click functionality and dom resets to the game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,6 +8,7 @@ $('document').ready(function(){
 
   //declare scores for the player and their goal
   var targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+  console.log(targetScore);
   var currentScore = 0;
 
   //declare win and loss trackers
@@ -23,4 +24,64 @@ $('document').ready(function(){
     currentScore = 0; 
   }
 
+  function checkScores(){
+    if (currentScore === targetScore) {
+      alert('You Win!');
+      wins++;
+      resetGame();
+      updateScores();
+
+    } else if (currentScore > targetScore) {
+      alert('You Lose!');
+      losses++;
+      resetGame();
+      updateScores();
+
+    }
+  }
+
+  function updateScores(){
+    $('.win-div').text(wins);
+    $('.lose-div').text(losses);
+    resetDom();
+  }
+
+  function resetDom(){
+    $('.ts-div').text(targetScore);
+    $('.cs-div').text(currentScore);
+  }
+
+  $('.blue-crystal').on('click', function(){
+    currentScore += blueCrystal;
+
+    $('.cs-div').text(currentScore);
+
+    checkScores();
+  })
+
+  $('.purple-crystal').on('click', function () {
+    currentScore += purpleCrystal;
+
+    $('.cs-div').text(currentScore);
+
+    checkScores();
+  })
+
+  $('.yellow-crystal').on('click', function () {
+    currentScore += yellowCrystal;
+
+    $('.cs-div').text(currentScore);
+
+    checkScores();
+  })
+
+  $('.red-crystal').on('click', function () {
+    currentScore += redCrystal;
+
+    $('.cs-div').text(currentScore);
+
+    checkScores();
+  })
+
+  updateScores();
 })
