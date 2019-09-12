4681ef0d66af97e785a60b40cacb9d5007457cae
race-briggs <race.briggs@gmail.com>
2019-09-12 15:08:05 -0500

    added console logs and comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,9 +2,13 @@ $('document').ready(function(){
 
   //declare crystals as random number values
   var blueCrystal = Math.floor(Math.random()*12) + 1;
+  console.log(blueCrystal);
   var purpleCrystal = Math.floor(Math.random() * 12) + 1;
+  console.log(purpleCrystal);
   var yellowCrystal = Math.floor(Math.random() * 12) + 1;
+  console.log(yellowCrystal);
   var redCrystal = Math.floor(Math.random() * 12) + 1;
+  console.log(redCrystal)
 
   //declare scores for the player and their goal
   var targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
@@ -15,15 +19,21 @@ $('document').ready(function(){
   var wins = 0;
   var losses = 0;
 
+  //resets variable values
   function resetGame(){
     blueCrystal = Math.floor(Math.random() * 12) + 1;
+    console.log(blueCrystal);
     purpleCrystal = Math.floor(Math.random() * 12) + 1;
+    console.log(purpleCrystal);
     yellowCrystal = Math.floor(Math.random() * 12) + 1;
+    console.log(yellowCrystal);
     redCrystal = Math.floor(Math.random() * 12) + 1;
+    console.log(yellowCrystal);
     targetScore = Math.floor(Math.random() * (120 - 19 + 1) + 19);
     currentScore = 0; 
   }
 
+  //checks for a win or loss
   function checkScores(){
     if (currentScore === targetScore) {
       alert('You Win!');
@@ -40,17 +50,20 @@ $('document').ready(function(){
     }
   }
 
+  //updates the scores on the HTML
   function updateScores(){
     $('.win-div').text(wins);
     $('.lose-div').text(losses);
     resetDom();
   }
 
+  //resets targetScore dom and currentScore dom
   function resetDom(){
     $('.ts-div').text(targetScore);
     $('.cs-div').text(currentScore);
   }
 
+  //crystal image click functions
   $('.blue-crystal').on('click', function(){
     currentScore += blueCrystal;
 
@@ -83,5 +96,6 @@ $('document').ready(function(){
     checkScores();
   })
 
+  //populates HTML with 0s for wins and losses on screen load
   updateScores();
 })
